cbcde3829c7435306bbfac86c681ab1a1742ef71
Jefferson Lopez <jeffolopez8@gmail.com>
2020-12-07 21:07:48 -0500

    Tarea semana uno prueba inicial de git

---
+++ src/semana1/Semana1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semana1;
+
+/**
+ *
+ * @author jeffo
+ */
+public class Semana1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        formPrincipal v_principal = new formPrincipal();
+        v_principal.show();
+    }
+    
+}
